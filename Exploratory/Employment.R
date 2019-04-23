961aa2303cbdcae6937969168c8724cd11dac6e9
SreejaSN <sreeja13@gmail.com>
2019-04-23 16:00:58 -0600

    added employment by race data

--- Exploratory/Employment.R
+++ Exploratory/Employment.R
@@ -5,6 +5,8 @@ library(leaflet)
 library(billboarder)
 library(plotly)
 library(scales)
+library(tidycensus)
+library(crayon)
 
 employ <- get(load("G:/My Drive/ACS/2017 5-year/acs_employ_femchild_lst.RData"))
 edu <- get(load("G:/My Drive/ACS/2017 5-year/acs_educ_over25years_lst.RData"))
@@ -66,6 +68,126 @@ pCount <- plot_ly(data = countyData, x = ~employedCounty$employment, y = ~employ
          xaxis = list(title = "Labor Status"),
          yaxis = list(title = "Value"))
 pCount
+
+
+
+
+# Employment by race ------------------------------------------------------
+
+acsvars <- load_variables(2017, "acs1", cache = T) %>%
+  mutate(level = str_count(label, pattern = "!!")) %>%
+  rowwise() %>%
+  mutate(levlab = str_split(label, pattern = "!!") %>% unlist() %>% .[level + 1]) %>%
+  ungroup() %>%
+  mutate(concept = str_to_title(concept)) %>%
+  dplyr::rename(variable = name)
+
+
+si_acs <- function(table, county = NULL, state = NULL, summary_var = "universe total", geography = NULL, survey = NULL) {
+  cat(yellow(bold("Reminder: You must stay within the same level for any summary to be valid!\n")))
+  
+  if(summary_var == "universe total") summary_var = paste0(table, "_001")
+  summary_label = acsvars %>% filter(variable == summary_var) %>% pull(levlab)
+  
+  get_acs(geography = geography,
+          table = table,
+          county = county,
+          state = state,
+          output = "tidy",
+          year = 2017,
+          cache_table = T,
+          summary_var = summary_var, 
+          geometry = FALSE, 
+          cb = FALSE, 
+          survey = survey) %>%
+    clean_names() %>%
+    left_join(acsvars) %>%
+    select(-summary_moe, -variable) %>%
+    select(geoid, county = name, level, levlab, estimate, everything()) %>%
+    rename(!!summary_label := summary_est)
+}
+
+
+si_acs("B23002I", geography = "county", state = "UT", survey = "acs1")
+
+# A White alone 
+# B Black or African American Alone
+# C American Indian and Alaska Native Alone
+# D Asian Alone
+# E Native Hawaiian and Other Pacific Islander Alone
+# F Some Other Race Alone
+# G Two or More Races
+# H White Alone, Not Hispanic or Latino 
+# I Hispanic or Latino 
+
+
+emp_df <- lst()
+  
+for(letter in c("A", "B", "C", "D", "E", "F", "G", "H", "I")) {
+    tablenum <- paste0("B23002", letter)
+    
+    df_temp <- si_acs(tablenum, geography = "county", state = "NC", county  = "Guilford County", survey = "acs1") %>% 
+      select(county, estimate, label, level) %>% 
+      mutate(race = letter) 
+    
+    emp_df[[letter]] <- df_temp
+}
+
+
+emp_race <- bind_rows(emp_df)
+
+emp_race <- emp_race %>% 
+  mutate(race = case_when(race == "A" ~ "White alone",
+                          race == "B"~ "Black or African American Alone",
+                          race == "C" ~ "American Indian and Alaska Native Alone",
+                          race == "D" ~ "Asian Alone",
+                          race == "E" ~ "Native Hawaiian and Other Pacific Islander Alone",
+                          race == "F" ~ "Some Other Race Alone",
+                          race == "G" ~ "Two or More Races",
+                          race == "H" ~ "White Alone, Not Hispanic or Latino", 
+                          race == "I" ~ "Hispanic or Latino")) %>% 
+  filter(race == "White alone"|race == "Black or African American Alone") %>% 
+  filter(!is.na(estimate)) %>% 
+  filter(str_detect(label, "Civilian")|str_detect(label,"Employed")|str_detect(label, "65 to 69")|str_detect(label, "70 years")) %>% 
+  filter(!str_detect(label, "Unemployed")) %>% 
+  filter(!str_detect(label, "Not in labor")) %>% 
+  separate(label, c("l1", "l2", "l3", "l4", "l5", "l6", "l7"), sep = "!!", remove = FALSE) %>% 
+  rename(sex = l3,
+         ages = l4,
+         laborforce = l5,
+         civ_staus = l6,
+         emp_status = l7) %>% 
+  select(-l1, -l2) %>% 
+  unite(sex_race, c(sex, race)) %>% 
+   mutate(emp_status = case_when(civ_staus == "Employed"~ "Employed", 
+                                TRUE~emp_status)) %>% 
+  mutate(laborforce = case_when(str_detect(ages, "65")|str_detect(ages, "70")~"In labor force",
+                                TRUE~laborforce)) %>% 
+  filter(!is.na(laborforce))  %>% 
+  filter(!(str_detect(ages, "70")&level ==4)) %>% 
+  filter(!(str_detect(ages, "65")&level ==4)) %>%
+  select(-civ_staus) %>% 
+  group_by(ages, sex_race) %>% 
+  arrange(emp_status, .by_group = TRUE) %>% 
+  mutate(perc = round(estimate/lead(estimate)*100),0) %>% 
+  ungroup() %>% 
+  filter(!is.na(perc)) %>% 
+  select(sex_race, perc, ages) %>% 
+  spread(sex_race, perc) 
+
+#save(emp_race, file = "G:/My Drive/SI/DataScience/data/Guilford County CIP/dashboard/emp_race.rda")
+
+
+
+billboarder() %>% 
+  bb_barchart(data = emp_race)
+ 
+
+
+
+
+
+
 
 
 
