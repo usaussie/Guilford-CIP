7333e8ab2f1386cdf1cae686126978a1b8943c30
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-05-12 15:43:44 -0400

    add some exploratory vis

--- Exploratory/Jasons Data/Permits.R
+++ Exploratory/Jasons Data/Permits.R
@@ -27,18 +27,18 @@ pm <- pm %>%
 #
 #     try(pb$tick()$print())
 #
-#     GET(url = "http://gis.guilfordcountync.gov/arcgis/rest/services/Geocode_Services/GCStrts_Parcels_Composite/GeocodeServer/findAddressCandidates",
-#         query = list(f = "json",
-#                      SingleLine = full_address,
-#                      outSR = "4326",
-#                      outFields = "StreetName, Match_addr",
-#                      maxLocations = 1)) %>%
-#       pluck("content") %>%
-#       rawToChar() %>%
-#       jsonlite::fromJSON() %>%
-#       pluck("candidates")
-#
-#     }))
+    # GET(url = "http://gis.guilfordcountync.gov/arcgis/rest/services/Geocode_Services/GCStrts_Parcels_Composite/GeocodeServer/findAddressCandidates",
+    #     query = list(f = "json",
+    #                  SingleLine = full_address,
+    #                  outSR = "4326",
+    #                  outFields = "StreetName, Match_addr",
+    #                  maxLocations = 1)) %>%
+    #   pluck("content") %>%
+    #   rawToChar() %>%
+    #   jsonlite::fromJSON() %>%
+    #   pluck("candidates")
+    #
+    # }))
 #
 # arcgis_geocoded <- geo_pull %>%
 #   mutate(json = map(json, ~ if(is.null(.x)) {
@@ -59,7 +59,38 @@ pm <- pm %>%
 #Center of Guilford County, NC:
 guilford <- c(36.086046, -79.796661)
 
-pm %>%
+### Get a map
+map <- get_map(location = c(lat = 36.086046, lon = -79.796661), zoom = 9,
+               maptype = "roadmap", source = "google")
+
+### When you draw a figure, you limit lon and lat.
+guilfordmap <- ggmap(map)
+
+pmtomap <- pm %>%
   filter(!is.na(permitfees)) %>%
-google_map(data = ., location = guilford) %>%
-  add_heatmap(lat = "lat", lon = "lon", weight = "permitfees")
+  mutate(apptype = str_remove(apptype, " Permit| Web Application")) %>%
+  mutate(permit_percentile = permitfees/max(permitfees))
+
+ggmap(map, base_layer = ggplot(pmtomap, aes(x = lon, y = lat))) +
+  scale_color_viridis(option = "plasma") + guides(fill = F, color = F, alpha = F, size = F) +
+  scale_fill_viridis() +
+  geom_density_2d() +
+  stat_density_2d(aes(fill = ..level.., alpha = .5), geom = "polygon") +
+  scale_x_continuous(limits = c(-80.25, -79.25), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(35.8, 36.4), expand = c(0, 0))
+
+
+ggmap(map, base_layer = ggplot(pmtomap, aes(x = lon, y = lat))) +
+  scale_color_viridis(option = "inferno") + guides(fill = F, color = F, alpha = F, size = F) +
+  scale_fill_viridis() +
+  geom_point(aes(size = permit_percentile, color =  permit_percentile, alpha = permit_percentile)) +
+  scale_x_continuous(limits = c(-80.25, -79.25), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(35.8, 36.4), expand = c(0, 0))
+
+ggmap(map, base_layer = ggplot(pmtomap, aes(x = lon, y = lat))) +
+  guides(fill = F, color = F, alpha = F, size = F) +
+  scale_fill_viridis() +
+  facet_wrap(~apptype) +
+  geom_point(aes(size = permit_percentile, color = apptype)) +
+  scale_x_continuous(limits = c(-80.25, -79.25), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(35.8, 36.4), expand = c(0, 0))
