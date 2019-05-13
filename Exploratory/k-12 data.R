31b7d26e05554e26384cdcbfe72c676c2a41c1ad
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-05-13 13:32:46 -0400

    fixed name issue much more elegantly.  Nothing like sleeping on it for a 3 Am fix.

--- Exploratory/k-12 data.R
+++ Exploratory/k-12 data.R
@@ -6,17 +6,12 @@ require(siverse)
 
 tests <- dir_ls(path_expand("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data"), glob = "*testresults.xlsx") %>%
   map(~read_excel(.x, skip = 6) %>%
-        add_column(year = parse_number(basename(.x)))
+        add_column(year = parse_number(basename(.x))) %>%
+        clean_names()
       ) %>%
   bind_rows() %>%
-  clean_names() %>%
   gather(key = percent_group, value = percent, contains("percent")) %>%
-  mutate(percent_group = str_remove(percent_group, "_2$"),
-         percent_group = case_when(percent_group == "percent_level" ~ "percent_level_2",
-                                   TRUE ~ percent_group)) %>%
   replace_with_na(list(percent = "*")) %>%
-  mutate(school_code = coalesce(school_code, school_code_2)) %>%
-  select(-school_code_2) %>%
   mutate(yearnum_start = as.numeric(str_sub(year, 1, 2))) %>%
   mutate(percent = parse_number(percent)) #change <5 to 5 and >095 to 95
 
