3a1e8f7d36b9d4718dc9c4b82c7a2c9a911f6438
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-05-16 20:47:40 -0400

    adding SPG

--- Exploratory/k-12 data.R
+++ Exploratory/k-12 data.R
@@ -21,3 +21,41 @@ tests %>%
   filter(percent_group == "percent_level_1") %>%
   ggplot(aes(x = yearnum_start, y = percent)) + #facet_wrap(~percent_group) +
   geom_point() + geom_smooth() + guides(fill = F, color = F)
+
+
+
+
+# School performance grade ----------------------------------------------------------------------------------------
+spg <- bind_rows(
+  read_excel(path_expand("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data/spg1314.xlsx"), skip = 3) %>%
+    add_column(year = 1314) %>%
+    clean_names() %>%
+    rename(district_name = lea_name,
+           state_board_region = sbe_region),
+
+  read_excel(path_expand("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data/spg1415.xlsx"), skip = 7) %>%
+    add_column(year = 1415) %>%
+    clean_names() %>%
+    rename(district_name = lea_name,
+           state_board_region = sbe_district),
+
+  read_excel(path_expand("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data/spg1516.xlsx"), skip = 6) %>%
+    add_column(year = 1516) %>%
+    clean_names(),
+
+  read_excel(path_expand("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data/spg1617.xlsx"), skip = 6) %>%
+    add_column(year = 1617) %>%
+    clean_names() %>%
+    rename(state_board_region = sbe_district)
+)
+
+spg <- spg %>%
+  mutate(state_board_region = str_remove(state_board_region, " Region"),
+         state_board_region = str_replace(state_board_region, "-", " "))
+
+spg <- spg %>%
+  filter(district_name == "Guilford County Schools")
+
+spg %>%
+  count(year, spg_grade) %>%
+  ggplot(aes)
