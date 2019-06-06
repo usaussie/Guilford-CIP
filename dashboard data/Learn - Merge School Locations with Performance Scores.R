886b55e3113b33cdc0bf6b677fe5a0e2fb633edf
SreejaSN <sreeja13@gmail.com>
2019-06-05 19:25:47 -0600

    trying fuzzy join

--- dashboard data/Learn - Merge School Locations with Performance Scores.R
+++ dashboard data/Learn - Merge School Locations with Performance Scores.R
@@ -1,5 +1,6 @@
 require(tidyverse)
 require(siverse)
+library(fuzzyjoin)
 
 spg <- read_rds("~/Google Drive/SI/DataScience/Data/Guilford County CIP/K-12 Data/spg.rds")
 schools <- read_rds("~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/schools.rds")
@@ -13,3 +14,20 @@ spg %>%
               filter(str_detect(name, fixed("school", ignore_case = T))) %>%
               arrange(name) %>%
               mutate(school_name = str_remove(name, " School$")))
+
+
+guilford_schools <- spg %>% 
+  filter(district_name == "Guilford County Schools") %>%
+  distinct(school_name)
+
+loc_schools <- schools %>%
+  filter(str_detect(name, fixed("school", ignore_case = T))) %>%
+  arrange(name) %>%
+  mutate(school_name = str_remove(name, " School$"))
+  
+
+fuz_join <- guilford_schools %>% stringdist_inner_join(loc_schools, by = c(school_name = "school_name"), max_dist = 1)
+
+
+
+
