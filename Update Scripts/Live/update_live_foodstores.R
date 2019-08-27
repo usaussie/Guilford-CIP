4d44f68fffab51557cd166970b69bbc05c58723e
Jon Zadra <jon.zadra@sorensonimpact.com>
2019-08-27 12:02:12 -0600

    done

--- Update Scripts/Live/update_live_foodstores.R
+++ Update Scripts/Live/update_live_foodstores.R
@@ -1,76 +1,77 @@
 require(tidyverse)
-require(siverse)
 require(googleway)
 options(warnPartialMatchArgs = F)
-#Author: Jon Zadra
-
-#Center of Guilford County, NC:
-  guilford <- c(36.086046, -79.796661)
-
-#This code is messy.  Google places only allows 20 results at a time (a "page") and then only up to 3 total pages (60 results total).  In order to make sure I'm not missing anything, I've generated a grid of locations around Guilford county, and then done a search around a radius of each point, getting as many pages as possible.  The reuslting list is expected to be full of duplicates.
-
-# Only uncomment and run the below code if you make a change - prevent using our API unless necessary.
 
 
+# Options ---------------------------------------------------------------------------------------------------------
 
-# place_types <- c("supermarket",
-#                  "grocery_or_supermarket")
-#
-#
-# bottomleft <- c(35.861792, -80.112286)
-# topright <-  c(36.239347, -79.583953)
-# bottomright <- c(35.910726, -79.580493)
-# topleft <- c(36.243619, -80.023036)
-#
-# lat <- topleft[1] - seq(0, topleft[1] - bottomleft[1], length.out = 5)
-# lon <- bottomleft[2] - seq(0,  bottomleft[2] - bottomright[2], length.out = 5)
-#
-# location_grid <- tibble(lat, lon) %>%
-#   expand(lat, lon)
-#
-# reslst <- lst()
-# iter <- 0
-#
-# for(h in 1:nrow(location_grid)) {
-#   location <- location_grid %>% slice(h) %>% as_vector()
-#   message(h)
-#
-#   for(j in seq(place_types)) {
-#     last_token <- NULL
-#
-#     for(i in 1:3) {
-#
-#       res <- google_places(location = location,
-#                                  radius = 20000,
-#                                  place_type = place_types[j],
-#                                  page_token = last_token)
-#
-#       results <- res$results
-#
-#       last_token <- res$next_page_token
-#
-#       iter <- iter + 1
-#
-#       reslst[[iter]] <- results %>% select(id, name, place_id, price_level, rating, user_ratings_total, types) %>% as_tibble() %>% add_column(iter = iter, place_type_search = place_types[j], location_grid = paste(location[1], location[2], sep = ", ")) %>% bind_cols(lat = results$geometry$location$lat, lon = results$geometry$location$lng)
-#
-#       if(is.null(last_token)) break
-#
-#       Sys.sleep(time = 3)
-#
-#
-#       }
-#   }
-# }
+# You will need to set the google api key using the following code in the console.   DO NOT SAVE THE KEY in this file, as it will be visible to anyone on github!  Contact Tara if you do not have the key.
 
-# write_rds(reslst, "~/Google Drive/SI/DataScience/data/Guilford County CIP/Food Stores/food stores full lst.rds")
-reslst <- read_rds("~/Google Drive/SI/DataScience/data/Guilford County CIP/Food Stores/food stores full lst.rds")
+set_key("your key here") #Run in console only!
+
+# Be sure that you have enabled the "Places API" in your developer console.
+
+
+
+# Data Pull -------------------------------------------------------------------------------------------------------
+
+
+#Google places only allows 20 results at a time (a "page") and then only up to 3 total pages (60 results total).  In order to make sure I'm not missing anything, I've generated a grid of locations around Guilford county, and then done a search around a radius of each point, getting as many pages as possible.  The reuslting list is expected to be full of duplicates.
+
+
+guilford <- c(36.086046, -79.796661) #Center of Guilford County, NC:
+
+place_types <- c("supermarket",
+                 "grocery_or_supermarket")
+
+#Define the bounds
+bottomleft <- c(35.861792, -80.112286)
+topright <-  c(36.239347, -79.583953)
+bottomright <- c(35.910726, -79.580493)
+topleft <- c(36.243619, -80.023036)
+
+#split them up
+lat <- topleft[1] - seq(0, topleft[1] - bottomleft[1], length.out = 5)
+lon <- bottomleft[2] - seq(0,  bottomleft[2] - bottomright[2], length.out = 5)
+
+location_grid <- tibble(lat, lon) %>%
+  expand(lat, lon)
+
+reslst <- lst()
+iter <- 0
+
+for(h in 1:nrow(location_grid)) { #for each location
+  location <- location_grid %>% slice(h) %>% as_vector()
+  message(h)
+
+  for(j in seq(place_types)) { #for each place type
+    last_token <- NULL
+
+    for(i in 1:3) { #each page of results
+
+      res <- google_places(location = location,
+                                 radius = 20000,
+                                 place_type = place_types[j],
+                                 page_token = last_token)
+
+      results <- res$results
+
+      last_token <- res$next_page_token
+
+      iter <- iter + 1
+
+      reslst[[iter]] <- results %>% select(id, name, place_id, price_level, rating, user_ratings_total, types) %>% as_tibble() %>% add_column(iter = iter, place_type_search = place_types[j], location_grid = paste(location[1], location[2], sep = ", ")) %>% bind_cols(lat = results$geometry$location$lat, lon = results$geometry$location$lng)
+
+      if(is.null(last_token)) break #if there is no next page because there were only 1 or 2 results pages
+
+      Sys.sleep(time = 3)
+
+
+      }
+  }
+}
 
 all_results <- bind_rows(reslst)
-
-all_results %>% count(place_id,  sort = T) %>% count(n)
-
-all_results %>% select(-types, -iter, -place_type_search) %>% distinct
-
 
 
 all_results <- all_results %>%
@@ -92,23 +93,23 @@ guilfordzips <- c(27263, 27214, 27233, 27235, 27249, 27401, 27403, 27405, 27406,
 
 results <- results %>% filter(zip %in% guilfordzips | is.na(zip))
 
-write_rds(results, "~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/food_stores.rds")
+
+# Manual removal of stores ----------------------------------------------------------------------------------------
+
+#Take a look at the results and see if any don't seem like what we're after - you may want to use google maps to look at photos and place descriptions.
+
+results %>% count(name, sort = T)
+
+results <- results %>%
+  filter(name != "Publix Event Planning at Westchester Square") %>%
+  filter(name != "The Fresh Market - Corporate Office") %>%
+  filter(name != "Cà Phê Hâi Âu") %>%
+  filter(name != "Iniciativa Duarte") %>%
+  filter(name != "Home Storage Center and Bishops' Storehouse") %>%
+  filter(name != "La Guadalupana") %>%
+  filter(name != "Little Mexico")
 
 
-google_map(data = results, location = guilford) %>%
-  add_markers()
-
-
-# Kendall's Updates -------------------------------------------------------
-
-#Food stores Updates
-
-original_food_stores <- read_rds("~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/food_stores.rds")
-view(original_food_stores)
-
-#Data exploration shows that many Family Dollar and Walmart stores are very close to each other - example 3 Family Dollar stores in High Point
-filter_food_stores <- original_food_stores %>%
-  filter(str_detect(name, "Publix Event Planning at Westchester Square", negate = TRUE) , str_detect(name, "Superior Foods Supermarket", negate = TRUE)
-         , str_detect(name, "The Fresh Market - Corporate Office", negate = TRUE))
-
-save(filter_food_stores, file = "~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/food_stores_1.rda")
+# save(filter_food_stores, file = "~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/food_stores_1.rda")
+#
+# write_rds(results, "~/Google Drive/SI/DataScience/data/Guilford County CIP/dashboard/food_stores.rds")
